0ab0c7d9bc20b16cf713504a0ccd67b32508fa90
Alex Dzyuba <alexeydzyuba27@gmail.com>
2023-10-04 13:57:02 +0300

    w

---
+++ public/js/form-cl.js
@@ -0,0 +1,82 @@
+class Input {
+  constructor(name, nameRussian, rules) {
+    this.name = name
+    this.nameRussian = nameRussian
+
+    this.rules = rules.map(R => new R(name, nameRussian))
+    this.rules.forEach(r => r.enforce())
+  }
+}
+
+class Rule {
+  // TODO: мне канеш не оч нравится что у меня повторяются name и nameRussian
+  // было бы неплохо сразу их иметь уже как-то
+  // Но походу это невозможно в моей текущей парадигме
+  constructor(name, nameRussian) {
+    this.name = name
+    this.nameRussian = nameRussian
+
+    this.input = document.getElementById(this.name)
+  }
+
+  errorMessage() {}
+  enforce() {}
+
+  showError() {
+    if (this.input.nextElementSibling?.classList.contains('error')) return
+
+    const html = `
+      <div class="error">${this.errorMessage()}</div>
+    `
+    this.input.insertAdjacentHTML('afterend', html)
+  }
+  hideError() {
+    if (this.input.nextElementSibling?.classList.contains('error')) {
+      this.input.nextElementSibling.remove()
+    }
+  }
+}
+class RuleRequired extends Rule {
+  errorMessage() {
+    return `Поле "${this.nameRussian}" должно быть заполнено`
+  }
+
+  enforce() {
+    this.input.addEventListener('blur', () => {
+      if (this.input.value.trim().length === 0) {
+        this.showError()
+      } else {
+        this.hideError()
+      }
+    })
+  }
+}
+class RuleMaxLength extends Rule {
+  constructor(name, nameRussian, maxLength = 20) {
+    super(name, nameRussian)
+    this.maxLength = maxLength
+  }
+
+  errorMessage() {
+    return `Поле "${this.nameRussian}" не должно быть больше ${this.maxLength} символов`
+  }
+
+  enforce() {
+    this.input.addEventListener('blur', () => {
+      if (this.input.value.length > this.maxLength) {
+        this.showError()
+      } else {
+        this.hideError()
+      }
+    })
+  }
+}
+
+
+const inputsWithValidationRules = [
+  new Input('firstName', 'имя', [RuleRequired, RuleMaxLength]),
+  new Input('secondName', 'фамилия', [RuleRequired, RuleMaxLength])
+]
+
+// possible for OR
+// enforcePhoneOrEmailRequiredRule() {... new Input(...) ...}
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,7 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
-  <script src="js/form.js" defer></script>
+{{--  <script src="js/form.js" defer></script>--}}
+  <script src="js/form-cl.js" defer></script>
   <script src="js/main.js" defer></script>
 
   <title>Form</title>
